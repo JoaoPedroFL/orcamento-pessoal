055b5edee5e9b9e2716ce9928b9c5adf38641d32
João Pedro <estudante2025@gmail.com>
2022-02-14 10:48:03 -0300

    Função Listagem de despesas criada

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ class Despesa {
     }
 
     validarDados() {
-        for(let i in this) {
-            if(this[i] == undefined || this[i] == '' || this[i] == null) {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false
             }
         }
@@ -23,7 +23,7 @@ class Bd {
     constructor() {
         let id = localStorage.getItem('id')
 
-        if(id === null) {
+        if (id === null) {
             localStorage.setItem('id', 0)
         }
     }
@@ -34,12 +34,37 @@ class Bd {
     }
 
     gravar(d) {
-        
+
         let id = this.getProximoId()
 
         localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+
+        //Array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas em localstorage
+        for (let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver indices que foram pulados ou removidos
+            //neste casos nós vamos pular essse indices
+            if (despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+
+        return despesas
     }
 }
 
@@ -56,7 +81,7 @@ function cadastrarDespesa() {
     let valor = document.getElementById('valor')
 
 
-    let despesa = new Despesa (
+    let despesa = new Despesa(
         ano.value,
         mes.value,
         dia.value,
@@ -65,8 +90,8 @@ function cadastrarDespesa() {
         valor.value
     )
 
-    if(despesa.validarDados()) {
-        
+    if (despesa.validarDados()) {
+
         bd.gravar(despesa)
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
@@ -75,6 +100,13 @@ function cadastrarDespesa() {
         document.getElementById('modal_botao').className = "btn btn-success"
 
         $('#modalRegistraDespesa').modal('show')
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
 
     } else {
 
@@ -85,4 +117,43 @@ function cadastrarDespesa() {
 
         $('#modalRegistraDespesa').modal('show')
     }
+}
+
+function carregaListaDespesas() {
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function (d) {
+
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo
+        switch (parseInt(d.tipo)) {
+
+            case 1: d.tipo = 'Alimentação'
+                break
+            case 2: d.tipo = 'Educação'
+                break
+            case 3: d.tipo = 'Lazer'
+                break
+            case 4: d.tipo = 'Saúde'
+                break
+            case 5: d.tipo = 'Transporte'
+                break
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
